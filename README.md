268a52c64e57bd028c16b6da771e93f25152b5ba
Adrian Rossiter <adrian@antiprism.com>
2022-04-13 22:17:08 +0200

    Separate and update cavacore, balance sample buffers, avoid sample loss

--- README.md
+++ README.md
@@ -32,22 +32,24 @@ To see all options run `cava_filter -h`
 ```
 Usage: cava_filter [options] [input_file]
 
-convert raw pcm_s16le format to frequency spectrum data using the cavacore
-library https://github.com/karlstav/cava. If input_file is not given the
-program reads from standard input.
+Convert raw pcm_s16le stereo 44100 format to frequency spectrum data using
+the cavacore library https://github.com/karlstav/cava. If input_file is not
+given the program reads from standard input.
 
   Options
-    -h,--help this help message
+  -h,--help this help message
   --version version information
 
   -b <num>   number of bars to print (default: 10)
-  -f <hz>    framerate in Hz (default: 25)
+  -f <hz>    framerate in Hz (default: 25) Note, the final frame will usually
+             be a partial frame and no bars value line will be printed for it
   -S         stereo output, print the right channel bars followed on the line
              by the left channel bars
-  -s <fact>  smooth factor, a number greater than 0.1 (not smooth) to set
-             spectrum smoothness (default: 1)
+  -n <fact>  noise reduction, a number between 0.0 noisy, and 1.0 smooth
+             (default: 0.1)
   -a <auto>  value for the cava autosens setting (default: 0 no autosens)
+  -c <frqs>  low and high cutoff frequencies for cava, two integers
+             separated by a comma (default: 50,10000)
   -F         the first line printed is the frequencies of the bands
   -o <file>  write output to file (default: write to standard output)
-
 ```
--- src/cava_filter.cpp
+++ src/cava_filter.cpp
@@ -25,7 +25,10 @@
 #include "programopts.hpp"
 #include "utils.hpp"
 
-#include "cavacore.h"
+extern "C" {
+#include "cavacore/cavacore.h"
+}
+
 #include <cerrno>
 #include <cmath>
 #include <cstdio>
@@ -33,92 +36,173 @@
 #include <cstring>
 
 class CavaFilter : public ProgramOpts {
-public:
-  int bars_per_channel = 10;
-  const int channels = 2;
-  int channels_out = 1;
-  const int height = 100;
-  double framerate = 25;
+private:
+  // Non-optional values
+  // input format must be pcm_s16le, stereo, 44100 sample rate, converted
+  // with, e.g. ffmpeg -i file.wav -f s16le -ar 44100 -acodec pcm_s16le -ac 2
+  const int channels = 2; // stereo
+  const int rate = 44100; // sample rate
   const size_t input_len_per_channel = 512;
-  const int rate = 44100;
+
+  // Optional values
+  int bars_per_channel = 10;
+  int channels_out = 1;
+  double framerate = 25;
   int autosens = 0;
-  double smooth_factor = 1; // 1: smooth, >1 more smooth, <1 less smooth
+  double noise_reduction = 0.1; // 0.0: noisy 1.0: smooth
   int print_freq_bands = false;
-  std::string in_file_name = "-";  // read from stdin
-  std::string out_file_name = "-"; // write to stdout
+  std::vector<int> cutoffs = {50, 10000}; // cava low_cutoff and high_cutoff
   FILE *in_file = stdin;
   FILE *out_file = stdout;
 
+  void print_freq_bands_line(float *freqs) const;
+  void print_freq_vals_line(const std::vector<double> &frame_bars) const;
+
+public:
   CavaFilter() : ProgramOpts("cava_filter") {}
+  ~CavaFilter();
   void process_command_line(int argc, char **argv);
   void usage();
+  Status generate_spectrum_file();
+};
 
-  size_t input_len() const { return input_len_per_channel * channels; }
-  size_t output_len() const { return bars_per_channel * channels; }
-  double cava_smoothing() const { return smooth_factor * rate / input_len(); }
-  struct cava_plan *get_cava_plan()
-  {
-    return cava_init(bars_per_channel, rate, channels, height, cava_smoothing(),
-                     autosens);
+CavaFilter::~CavaFilter()
+{
+  if (in_file != stdin) {
+    fclose(in_file);
+    in_file = stdin;
   }
-  void print_freq_bands_line(struct cava_plan *plan) const
-  {
-    if (print_freq_bands) {
-      for (int ch = 0; ch < channels_out; ch++)
-        for (int i = 0; i < bars_per_channel; i++)
-          fprintf(out_file, "%4d ", (int)plan->cut_off_frequency[i]);
-      fprintf(out_file, "\n");
-    }
+  if (out_file != stdout) {
+    fclose(out_file);
+    out_file = stdin;
   }
-  void print_freq_vals_line(double *frame_bars) const
-  {
-    int num_bars_out = bars_per_channel * channels_out;
-    for (int i = 0; i < num_bars_out; i++) {
-      double bar_ht =
-          (channels_out == 2)
-              ? frame_bars[i]
-              : (frame_bars[i] + frame_bars[i + bars_per_channel]) / 2;
-      fprintf(out_file, "%4d ", (int)bar_ht);
-    }
+}
+
+void CavaFilter::print_freq_bands_line(float *freqs) const
+{
+  if (print_freq_bands) {
+    for (int ch = 0; ch < channels_out; ch++)
+      for (int i = 0; i < bars_per_channel; i++)
+        fprintf(out_file, "%4d ", (int)freqs[i]);
     fprintf(out_file, "\n");
   }
+}
 
-  // discarding the fractional sample could lead to worst case
-  // desynchronization of approx .1 seconds per hour
-  int samples_per_frame() const
-  {
-    return (double)(rate * channels) / framerate;
+void CavaFilter::print_freq_vals_line(
+    const std::vector<double> &frame_bars) const
+{
+  int num_bars_out = bars_per_channel * channels_out;
+  for (int i = 0; i < num_bars_out; i++) {
+    double bar_ht =
+        (channels_out == 2)
+            ? frame_bars[i]
+            : (frame_bars[i] + frame_bars[i + bars_per_channel]) / 2;
+    fprintf(out_file, "%4d ", (int)bar_ht);
+  }
+  fprintf(out_file, "\n");
+}
+
+Status CavaFilter::generate_spectrum_file()
+{
+  Status stat;
+  auto *plan = cava_init(bars_per_channel, rate, channels, autosens,
+                         noise_reduction, cutoffs[0], cutoffs[1]);
+
+  if (print_freq_bands)
+    print_freq_bands_line(plan->cut_off_frequency);
+
+  size_t input_len = input_len_per_channel * channels; // samples buffier len
+  int bars_total = bars_per_channel * channels; // total bar vals in cava_out
+
+  std::vector<int16_t> cava_in_int16(input_len); // raw sample buffer
+  std::vector<double> cava_in(input_len);        // double sample buffer
+  std::vector<double> cava_out(bars_total);      // cava exec bar values
+  std::vector<double> frame_bars(bars_total);    // frame bar values
+
+  const double samples_per_frame = (double)(rate * channels) / framerate;
+  // '+ 1' sample to hold fractional part of sample
+  int execs_per_frame = ceil((samples_per_frame + 1) / input_len);
+  int samples_per_exec = samples_per_frame / execs_per_frame;
+  int samples_remainder =
+      samples_per_frame - execs_per_frame * samples_per_exec;
+
+  // Find the fractional part of the sample that would be lost each frame
+  double intpart;
+  double sample_fraction_per_frame = modf(samples_per_frame, &intpart);
+
+  double current_accumulated_sample_fractions = 0.0;
+  int finished = 0; // has all the data been read (or error)
+  while (!finished) {
+    for (int bar_idx = 0; bar_idx < bars_total; bar_idx++)
+      frame_bars[bar_idx] = 0;
+
+    for (int read_idx = 0; read_idx < execs_per_frame; read_idx++) {
+      // Add 1 to each of the first execs to include the samples remainder
+      size_t read_len = samples_per_exec + (read_idx < samples_remainder);
+
+      // Add an extra sample if needed to the last exec. There should always
+      // be room for this from the calculation of execs_per_frame
+      if (read_idx == execs_per_frame - 1) {
+        current_accumulated_sample_fractions += sample_fraction_per_frame;
+        if (current_accumulated_sample_fractions >= 1.0) {
+          read_len += 1;
+          current_accumulated_sample_fractions -= 1;
+        }
+      }
+
+      size_t actual_num_read =
+          fread(cava_in_int16.data(), sizeof(int16_t), read_len, in_file);
+      if (actual_num_read < read_len) { // end of stream or error
+        finished = 1;
+        if (ferror(in_file))
+          stat.set_error(std::string("reading input: ") + strerror(errno));
+        break;
+      }
+
+      // convert samples to doubles for cava
+      for (size_t i = 0; i < read_len; i++)
+        cava_in[i] = (int)cava_in_int16[i];
+
+      cava_execute(cava_in.data(), read_len, cava_out.data(), plan);
+
+      // add weighted bar values
+      for (int bar_idx = 0; bar_idx < bars_total; bar_idx++)
+        frame_bars[bar_idx] += cava_out[bar_idx] / execs_per_frame;
+    }
+
+    if (finished) // end of stream or error
+      break;
+
+    print_freq_vals_line(frame_bars);
   }
 
-  FILE *get_in_file() const { return in_file; }
-  FILE *get_out_file() const { return out_file; }
-  void close_files()
-  {
-    if (in_file != stdin)
-      fclose(in_file);
-    if (out_file != stdout)
-      fclose(out_file);
-  }
-};
+  cava_destroy(plan);
+  free(plan);
+
+  return stat;
+}
 
 void CavaFilter::usage()
 {
   fprintf(stdout, R"(
 Usage: %s [options] [input_file]
 
-convert raw pcm_s16le format to frequency spectrum data using the cavacore
-library https://github.com/karlstav/cava. If input_file is not given the
-program reads from standard input.
+Convert raw pcm_s16le stereo 44100 format to frequency spectrum data using
+the cavacore library https://github.com/karlstav/cava. If input_file is not
+given the program reads from standard input.
 
   Options
-  %s
+%s
   -b <num>   number of bars to print (default: 10)
-  -f <hz>    framerate in Hz (default: 25)
+  -f <hz>    framerate in Hz (default: 25) Note, the final frame will usually
+             be a partial frame and no bars value line will be printed for it
   -S         stereo output, print the right channel bars followed on the line
              by the left channel bars
-  -s <fact>  smooth factor, a number greater than 0.1 (not smooth) to set
-             spectrum smoothness (default: 1)
+  -n <fact>  noise reduction, a number between 0.0 noisy, and 1.0 smooth
+             (default: 0.1)
   -a <auto>  value for the cava autosens setting (default: 0 no autosens)
+  -c <frqs>  low and high cutoff frequencies for cava, two integers
+             separated by a comma (default: 50,10000)
   -F         the first line printed is the frequencies of the bands
   -o <file>  write output to file (default: write to standard output)
 
@@ -130,10 +214,11 @@ void CavaFilter::process_command_line(int argc, char **argv)
 {
   opterr = 0;
   int c;
+  std::string file_name;
 
   handle_long_opts(argc, argv);
 
-  while ((c = getopt(argc, argv, ":ho:b:f:Ss:a:F")) != -1) {
+  while ((c = getopt(argc, argv, ":ho:b:f:Sn:a:c:F")) != -1) {
     if (common_opts(c, optopt))
       continue;
 
@@ -146,18 +231,18 @@ void CavaFilter::process_command_line(int argc, char **argv)
 
     case 'f':
       print_status_or_exit(read_double(optarg, &framerate), c);
-      if (framerate < 1)
-        error("framerate must be greater than 1", c);
+      if (framerate <= 0)
+        error("framerate must be greater than 0", c);
       break;
 
     case 'S':
       channels_out = 2;
       break;
 
-    case 's':
-      print_status_or_exit(read_double(optarg, &smooth_factor), c);
-      if (smooth_factor <= 0)
-        error("smooth_factor must be a positive number", c);
+    case 'n':
+      print_status_or_exit(read_double(optarg, &noise_reduction), c);
+      if (noise_reduction < 0.0 || noise_reduction > 1.0)
+        error("noise reduction must be between 0.0 and 1.0", c);
       break;
 
     case 'a':
@@ -166,12 +251,33 @@ void CavaFilter::process_command_line(int argc, char **argv)
         error("autosens cannot be negative (0 to disable)", c);
       break;
 
+    case 'c':
+      // read two positive integers
+      print_status_or_exit(read_int_list(optarg, cutoffs, false, 2), c);
+      if (cutoffs.size() < 2)
+        error("must specify two cutoffs (low and high)", c);
+      if (cutoffs[0] < 1)
+        error("first cutoff (low) must be greater than 0", c);
+      if (cutoffs[1] <= cutoffs[0])
+        error("second cutoff (high) must be greater than first cutoff (low)",
+              c);
+      break;
+
     case 'F':
       print_freq_bands = true;
       break;
 
     case 'o':
-      out_file_name = optarg;
+      file_name = optarg;
+      if (file_name == "-") {
+        out_file = stdout;
+      }
+      else {
+        out_file = fopen(file_name.c_str(), "w");
+        if (!out_file)
+          error("could not open file for writing '" + file_name +
+                "': " + strerror(errno));
+      }
       break;
 
     default:
@@ -182,26 +288,13 @@ void CavaFilter::process_command_line(int argc, char **argv)
   if (argc - optind > 1)
     error("too many arguments");
 
-  if (argc - optind == 1)
-    in_file_name = argv[optind];
-
-  if (in_file_name == "-") {
+  file_name = (argc - optind == 0) ? "-" : argv[optind];
+  if (file_name == "-")
     in_file = stdin;
-  }
   else {
-    in_file = fopen(in_file_name.c_str(), "r");
+    in_file = fopen(file_name.c_str(), "r");
     if (!in_file)
-      error("could not open file for reading '" + in_file_name +
-            "': " + strerror(errno));
-  }
-
-  if (out_file_name == "-") {
-    out_file = stdout;
-  }
-  else {
-    out_file = fopen(out_file_name.c_str(), "w");
-    if (!out_file)
-      error("could not open file for writing '" + out_file_name +
+      error("could not open file for reading '" + file_name +
             "': " + strerror(errno));
   }
 }
@@ -210,84 +303,7 @@ int main(int argc, char *argv[])
 {
   CavaFilter cava;
   cava.process_command_line(argc, argv);
-
-  auto *plan = cava.get_cava_plan();
-
-  if (cava.print_freq_bands)
-    cava.print_freq_bands_line(plan);
-
-  size_t input_len = cava.input_len(); // samples per execute
-  size_t output_len = cava.output_len();
-
-  int16_t *cava_in_int16 = (int16_t *)malloc(input_len * sizeof(int16_t));
-  double *cava_in = (double *)malloc(input_len * sizeof(double));
-  double *cava_out = (double *)malloc(output_len * sizeof(double));
-  double *frame_bars = (double *)malloc(output_len * sizeof(double));
-
-  for (size_t i = 0; i < output_len; i++) {
-    cava_out[i] = 0;
-  }
-
-  // number of full buffer reads per frame
-  int reads_per_frame = cava.samples_per_frame() / input_len;
-  // length of final buffer read
-  int final_input_len = cava.samples_per_frame() - reads_per_frame * input_len;
-  // exact number of buffer reads in floating point
-  double reads_per_frame_exact = (double)cava.samples_per_frame() / input_len;
-  // convert to total buffer reads per frame
-  reads_per_frame = reads_per_frame + (final_input_len > 1);
-  // total number of bar values in cava_out
-  int bars_total = cava.output_len();
-
-  int finished = 0; // has all the data been read (or error)
-  while (!finished) {
-    for (int bar_idx = 0; bar_idx < bars_total; bar_idx++)
-      frame_bars[bar_idx] = 0;
-
-    for (int read_idx = 0; read_idx < reads_per_frame; read_idx++) {
-      // read full buffer, or partial buffer if it is a final partial read
-      size_t read_len = input_len;
-      if (read_idx == reads_per_frame - 1 && final_input_len)
-        read_len = final_input_len;
-
-      // input format pcm_s16le, converted with, e.g.
-      // ffmpeg -i file.wav -f s16le -ar 44100 -acodec pcm_s16le -ac 2 file.raw
-      size_t actual_num_read =
-          fread(cava_in_int16, sizeof(int16_t), read_len, cava.get_in_file());
-      if (actual_num_read < read_len) { // end of stream or error
-        finished = 1;
-        break;
-      }
-
-      // convert samples to doubles for cava
-      for (size_t i = 0; i < read_len; i++)
-        cava_in[i] = (int)cava_in_int16[i];
-
-      cava_execute(cava_in, read_len, cava_out, plan);
-
-      // add weighted bar values
-      double weight = (double)(read_len) / input_len;
-      for (int bar_idx = 0; bar_idx < bars_total; bar_idx++)
-        frame_bars[bar_idx] += weight * cava_out[bar_idx];
-    }
-
-    if (finished) // end of stream or error
-      break;
-
-    // weighted average of bars
-    for (int i = 0; i < bars_total; i++)
-      frame_bars[i] /= reads_per_frame_exact;
-
-    // print average of L and R channels
-    cava.print_freq_vals_line(frame_bars);
-  }
-
-  cava.close_files();
-  cava_destroy(plan);
-  free(plan);
-  free(cava_in);
-  free(cava_out);
-  free(frame_bars);
+  cava.print_status_or_exit(cava.generate_spectrum_file());
 
   return 0;
 }
